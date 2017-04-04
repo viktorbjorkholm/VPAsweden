c5009bc7285fe42814bed8be5b18be1b6b609d04
hakkerman <hedwigidaakerman@gmail.com>
2017-04-04 20:27:50 +0200

    Add files via upload

---
+++ regstudent.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+ 
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>My site</title>
+
+	  <!-- länk till stylesheet -->
+	<link rel="stylesheet" type="text/css" href="VPACSS.css">
+	
+    <style type="text/css">
+    
+    </style>
+
+  </head>
+  <body>
+   <img src="Bilder/indexbild.jpg" class="bg">
+		</img>
+   <div class="flex-container">
+   <div class="headermain">
+   <header>
+		 <div class="indexmeny">
+		<ul>
+  <li><a href="#home"> <button type="button" class="btn btn-warning">Meny</button></a></li>
+  <li><a href="#news">  <button type="button" class="btn btn-warning" >Logga in</button></a></li>
+
+</ul>
+		 </div>
+	</header>
+	
+	</div>
+		<main>
+		
+		<div class="mainform">
+		<div class="regform">
+	<form>
+		<div class="form-group">
+     
+      <input type="text" class="form-control" id="for" placeholder="Förnamn" style="float: left;
+    width: 48%; margin-bottom:15px;">
+    </div>
+    <div class="form-group">
+      
+      <input type="password" class="form-control" id="eft" placeholder="Efternamn" style="float: right;
+    width: 48%;">
+    </div>
+	
+	 <div class="form-group">
+      
+      <input type="password" class="form-control" id="eft" placeholder="E-mailadress" >
+    </div> 
+	
+	 <div class="form-group">
+	<input type="text" class="form-control" id="for" placeholder="lösenord" style="float: left;
+    width: 48%; margin-bottom:15px;">
+    </div>
+    <div class="form-group">
+      
+      <input type="password" class="form-control" id="eft" placeholder="lösenord" style="float: right;
+    width: 48%;">
+    </div>
+  </form>
+		
+	
+		</div>
+		</div>
+		
+		
+		</main>
+		<footer>
+		
+		LOL
+		</footer>
+		
+		
+	</div>
+  </body>
+</html>
